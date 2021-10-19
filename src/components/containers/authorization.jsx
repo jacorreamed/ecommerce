7574aec6258a710a13fd16b0259d528b1b09fa1f
jacorreamed <jacorreamed@gmail.com>
2021-10-19 17:09:54 -0500

    [cambio de priueba]

--- src/components/containers/authorization.jsx
+++ src/components/containers/authorization.jsx
@@ -1,4 +1,5 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
+import jwt_decode from 'jwt-decode';
 import Frame from "../views/frame";
 import SingleHeader from "../views/singleHeader";
 import Selector from "../views/auth/selector"
@@ -7,20 +8,39 @@ import SignIn from "../views/auth/signin"
 const Authorization = ()=>{
     const [typeAction,setTypeAction] = useState("signin");
 
-    const responseGoogle = (response) => {
-      console.log(response);
-    }
+    const LoginButtonRef = React.createRef();
 
     const handleClick = (type)=>{
       console.log(type)
       setTypeAction(type)
     }
-    
+
+    const handleCredentialResponse = (data)=>{
+      //console.log(data.credential);
+
+      const decode_token = jwt_decode(data.credential);
+
+      console.log(jwt_decode(data.credential));
+
+      //guardamos los datos de la sesion en sessionStorage
+    }
+
+    useEffect(() =>{
+
+      window.google.accounts.id.initialize({
+        client_id: '209857278123-nrlqej2c9uffo5bot6l2npg2mme7fgch.apps.googleusercontent.com',
+        callback: handleCredentialResponse
+      });
+
+      window.google.accounts.id.renderButton(LoginButtonRef.current,{});
+      //google.accounts.id.prompt();
+    },[])
+
     
     return (
         <Frame renderHeader={<SingleHeader/>}>
           <Selector handleClick={handleClick}/>
-          <SignIn responseGoogle={responseGoogle} typeAction={typeAction}/>
+          <SignIn typeAction={typeAction}  loginButtonRef={LoginButtonRef}/>
         </Frame>
     )
 }
